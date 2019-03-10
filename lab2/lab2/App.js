61f2f08374a9ecadc5c1c080bfbfda928d2e6737
Wize <Wize@knights.ucf.edu>
2019-03-10 14:02:52 -0400

    lab2
    Requirements for lab2. UCF Mobile Application Development.

---
+++ lab2/lab2/App.js
@@ -0,0 +1,98 @@
+import React from 'react'; //Imports packages that you'll be using.
+import {
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  View,
+  TextInput,
+  Button
+} from 'react-native';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: '', nameAvailable: false,isValid: true}
+    this.onPress = this.onPress.bind(this);
+    this.onChange = this.onChange.bind(this);
+  }
+  onChange = (text) => {this.setState({value: text});
+  }
+ // Validation for Regex using conditional statements.
+  onPress(event) {
+
+    if (/^[A-Za-z]+$/.test(this.state.value)) {
+      this.setState({
+        nameAvailable: true
+      });
+
+    } else {
+      this.setState({
+        isValid: false
+      });
+
+    }
+    event.preventDefault();
+  }
+
+  render() {
+
+    let returnArray = []
+    if (!this.state.nameAvailable) {
+      returnArray.push(
+
+        <View key = "1" style = {styles.container} flexDirection = "column" alignItems = 'stretch' >
+        <View > < TextInput key = "3" style = {styles.textInput} value = {this.state.value} onChangeText = {this.onChange} placeholder = "Enter your name" > < /TextInput></View >
+        <TouchableOpacity key = "2" style = {styles.buttonStyle}
+        onPress = {this.onPress} > < Text style = {styles.buttonText} > Submit < /Text></TouchableOpacity >
+        </View>
+      );
+      //conditional statement to test if users input is accurate and passes the requested characters.
+      if (!this.state.isValid)
+        returnArray.push(
+          <Text key = "error" style = {styles.error} > Must be a valid a - z or A - Z!< /Text>
+        );
+      return returnArray;
+    } else {
+      return ( <View> <Text style = {styles.right}> Greetings, salutations and welcomings,  {this.state.value} < /Text></View > );
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  buttonText: {
+    color: "white",
+    fontSize: 40
+  },
+  buttonStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'blue',
+    height: 75,
+    margin: 30,
+  },
+  textInput: {
+    margin: 30,
+    height: 75,
+    fontSize: 20
+  },
+  error: {
+    color: "red",
+    margin: 30,
+    height: 75,
+    fontSize: 20
+  },
+  right: {
+    margin: 30,
+    height: 75,
+    fontSize: 20
+  },
+  defaultText: {
+    fontSize: 20
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

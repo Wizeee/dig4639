bcaa60611ae2cde05b348a76f4e9c27cdc75e880
Wize <Wize@knights.ucf.edu>
2019-04-22 04:52:48 -0400

    ms1
    ms1

--- Final/AppNavigator.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
-
-import MainStackNavigator from './MainStackNavigator';
-
-export default createAppContainer(MainStackNavigator);
---
+++ final/screens/Detail.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default class Detail extends React.Component {
+	render () {
+		const {navigation} = this.props;
+		const dnd = navigation.getParam('dnd', '');
+
+		return (
+			<View style={styles.container}>
+				<Text style={styles.getStartedText}>Dungeons and Dragons Class: {JSON.stringify(dnd)}</Text>
+			</View>
+		)
+	}
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  getStartedText: {
+    fontSize: 20,
+    color: 'rgba(96,100,109, 1)',
+    lineHeight: 24,
+    textAlign: 'center',
+    marginTop: 10,
+    marginBottom: 15,
+  },
+});
